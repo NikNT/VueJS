dc08474cb3c9938157f58401b69f2656eb0cb86f
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2023-11-28 18:02:35 -0500

    4. Outputting Lists, Attribute Binding, Dynamic Classes, and Computed Properties

--- vue-firebase/app.js
+++ vue-firebase/app.js
@@ -3,6 +3,27 @@ const app = Vue.createApp({
   data() {
     return {
       showBooks: true,
+      url: "http://www.google.com",
+      books: [
+        {
+          title: "Name of the Wind",
+          author: "Patrick Rothfuss",
+          img: "assets/1.jpg",
+          isFav: true,
+        },
+        {
+          title: "The Way of Kings",
+          author: "Brandon Sanderson",
+          img: "assets/2.jpg",
+          isFav: false,
+        },
+        {
+          title: "The Final Empire",
+          author: "Brandon Sanderson",
+          img: "assets/3.jpg",
+          isFav: true,
+        },
+      ],
       title: "The Final Empire",
       author: "Brandon Sanderson",
       age: 45,
@@ -27,6 +48,9 @@ const app = Vue.createApp({
     handleMousemove(e) {
       this.x = e.offsetX;
       this.y = e.offsetY;
+    },
+    toggleFav(index) {
+      this.books[index].isFav = !this.books[index].isFav;
     },
   },
 });
